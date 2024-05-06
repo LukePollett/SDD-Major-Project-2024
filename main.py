f5d3d9e7bc9f812be8bf226eff2b6bfbc5cdd01f
Luke <lpollett@student.saintaug.nsw.edu.au>
2024-05-06 12:38:59 +1000

    From 17/12/23 - Minor bug fixes/changes since 23rd Nov + Began Analog Clock implementation

--- main.py
+++ main.py
@@ -3,94 +3,76 @@ import tkinter as tk
 from time import time, ctime
 from random import randint
 from datetime import datetime
+import math
 
-
-class ExamTimer:
-    
-    """
-    Aspects of the program:
-    - main_window: tk.Tk()
-        The main window of the program
-
-    - canvas: tk.Canvas()
-        The canvas on which the local time & the exam timer is displayed
-        This will be the the only window that is displayed to the students during the exam along with the controls
-            ^ this avoids confusion and prevents the students from getting distracted
-
-    - clock: tk.Label()
-        The label that displays the local time
-
-    - timing_canvas: tk.Canvas()
-        The canvas on which the controls are displayed
-
-    - start_button: tk.Button()
-        The button that starts the timer
-
-    - stop_button: tk.Button()
-        The button that stops/pauses the timer
-
-    - reset_button: tk.Button()
-        The button that resets the timer
-
-    - settings_button: tk.Button()
-        The button that opens the settings window for the NESA official/Administrator supervising the exam
-
-    - local_time_clock(): function 
-        Updates the local time every second, displayed on the canvas
-
-    """
+class ExamTimer():
 
     # Initialize the main window
     def __init__(self):
         self.main_window = tk.Tk()
-        self.main_window.geometry("800x500")
+        self.main_window.geometry("800x600")
         self.main_window.title("ExamTimer")
 
         # Create the canvas for displaying the grid
         self.canvas = tk.Canvas(self.main_window, width=700, height=600, bg="white")
         self.canvas.pack(side=tk.LEFT)
-        self.clock = tk.Label(self.canvas, text=ctime(), font=('Helvetica Bold', 50), bg="white", 
+
+        self.clock = tk.Label(self.canvas, text=ctime(), font=('Helvetica Bold', 75), bg="white", 
                               fg="black", borderwidth=3, relief="groove")
-        self.clock.place(relx=0.5, rely=0.1, anchor="n")
+        self.clock.place(relx=0.5, rely=0.7, anchor="n")
 
         # Create the canvas for the controls (start, stop, reset, settings)
         self.timing_canvas = tk.Canvas(self.main_window, width=100, height=100)
         self.timing_canvas.pack(side=tk.RIGHT)
 
-        self.start_button = tk.Button(self.timing_canvas, text="Start", bg="Green", width=100, height=5).pack()
+        #  Create the buttons for the controls
+        self.start_button = tk.Button(self.timing_canvas, text="Start", bg="Green", width=100, height=5).pack(pady=5)
 
-        self.stop_button = tk.Button(self.timing_canvas, text="Stop", bg="Red", width=100, height=5).pack()
+        self.stop_button = tk.Button(self.timing_canvas, text="Stop", bg="Red", width=100, height=5).pack(pady=5)
 
-        self.reset_button = tk.Button(self.timing_canvas, text="Reset", width=100, height=5).pack()
+        self.reset_button = tk.Button(self.timing_canvas, text="Reset", width=100, height=5).pack(pady=5)
 
-        self.settings_button = tk.Button(self.timing_canvas, text="Settings", width=100, height=5).pack()
+        self.settings_button = tk.Button(self.timing_canvas, text="Settings", width=100, height=5).pack(pady=5)
 
-        def local_time_clock():
+        # Create the hands of the analog clock
+        self.canvas.create_line(350, 200, 350, 35, fill="red", width=5)
+        
+        # Contains both the digital and analog clocks that display the computer's local time (ctime())
+        def local_time_clocks():
             local_time = ctime()
             digital_clock = local_time[11:19]
             hours = int(local_time[11:13])
 
+            # Convert to 12-Hour Time
             if hours > 12:
                 new_hours = hours - 12
-                digital_clock = digital_clock.replace(local_time[11:13], str(new_hours), 1)
+                digital_clock = digital_clock.replace(local_time[11:13], str(new_hours))
                 digital_clock += " pm"
 
             elif hours == 12:
-                digi_clock += " pm"
+                digital_clock += " pm"
 
             elif hours == 0:
                 new_hours = hours + 12
-                digital_clock = digital_clock.replace(local_time[11:13], str(new_hours), 1)
+                digital_clock = digital_clock.replace(local_time[11:13], str(new_hours))
+                digital_clock += " am"
+            
+            elif hours < 12:
                 digital_clock += " am"
 
-
+            # Update the Digital Clock every second
             self.clock['text'] = digital_clock
-            self.main_window.after(1000, local_time_clock)
+            self.main_window.after(1000, local_time_clocks)
+
+            # Create the Analog Clock
+            # Extract the seconds part from ctime(), convert it to an integer, and then convert it to radians
+            # The seconds hand will be drawn using the radians value
+            
 
         def exam_timer():
             pass
 
-        local_time_clock()
+        local_time_clocks()
 
         # Run the main window
         self.main_window.mainloop()
