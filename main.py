e79b3777013e3c55b161d1bcdfed7e13638ec004
Luke <lpollett@student.saintaug.nsw.edu.au>
2024-05-06 12:34:14 +1000

    From 23/11/23 - Digital Clock in Window + Implemented OOP (Class) + Made test.py file + Created am/pm on clock

--- main.py
+++ main.py
@@ -1,42 +1,103 @@
+# Implemented Python Libraries:
 import tkinter as tk
-from tkinter import *
-from time import ctime, sleep
+from time import time, ctime
 from random import randint
+from datetime import datetime
 
-# OFFICIAL IDEA: Make the HSC Clock thingo better
 
-# Create the main window
-main_window = tk.Tk()
-main_window.geometry("700x500")
-main_window.title("HSC Timer")
+class ExamTimer:
+    
+    """
+    Aspects of the program:
+    - main_window: tk.Tk()
+        The main window of the program
 
-# Create the canvas for displaying the grid
-canvas = tk.Canvas(main_window, width=500, height=500, bg="white")
-canvas.pack(side=tk.LEFT)
-clock = tk.Label(canvas, text=ctime(), font=('Arial', 25), bg="white", 
-                 fg="black", borderwidth=3, relief="solid")
-clock.place(relx=0.5, rely=0.5, anchor="center")
+    - canvas: tk.Canvas()
+        The canvas on which the local time & the exam timer is displayed
+        This will be the the only window that is displayed to the students during the exam along with the controls
+            ^ this avoids confusion and prevents the students from getting distracted
 
-# Create the frame for the controls
-controls_frame = tk.Frame(main_window, padx=10, pady=10)
-controls_frame.pack(side=tk.RIGHT)
+    - clock: tk.Label()
+        The label that displays the local time
 
-# Create the controls
-start_button = tk.Button(controls_frame, text="Start")
-start_button.pack(pady=5)
+    - timing_canvas: tk.Canvas()
+        The canvas on which the controls are displayed
 
-stop_button = tk.Button(controls_frame, text="Stop")
-stop_button.pack(pady=5)
+    - start_button: tk.Button()
+        The button that starts the timer
 
-reset_button = tk.Button(controls_frame, text="Reset")
-reset_button.pack(pady=5)
+    - stop_button: tk.Button()
+        The button that stops/pauses the timer
 
-def update_time():
-    clock['text'] = ctime()
-    main_window.after(1000, update_time)
+    - reset_button: tk.Button()
+        The button that resets the timer
 
-update_time()
+    - settings_button: tk.Button()
+        The button that opens the settings window for the NESA official/Administrator supervising the exam
 
-main_window.mainloop()
+    - local_time_clock(): function 
+        Updates the local time every second, displayed on the canvas
+
+    """
+
+    # Initialize the main window
+    def __init__(self):
+        self.main_window = tk.Tk()
+        self.main_window.geometry("800x500")
+        self.main_window.title("ExamTimer")
+
+        # Create the canvas for displaying the grid
+        self.canvas = tk.Canvas(self.main_window, width=700, height=600, bg="white")
+        self.canvas.pack(side=tk.LEFT)
+        self.clock = tk.Label(self.canvas, text=ctime(), font=('Helvetica Bold', 50), bg="white", 
+                              fg="black", borderwidth=3, relief="groove")
+        self.clock.place(relx=0.5, rely=0.1, anchor="n")
+
+        # Create the canvas for the controls (start, stop, reset, settings)
+        self.timing_canvas = tk.Canvas(self.main_window, width=100, height=100)
+        self.timing_canvas.pack(side=tk.RIGHT)
+
+        self.start_button = tk.Button(self.timing_canvas, text="Start", bg="Green", width=100, height=5).pack()
+
+        self.stop_button = tk.Button(self.timing_canvas, text="Stop", bg="Red", width=100, height=5).pack()
+
+        self.reset_button = tk.Button(self.timing_canvas, text="Reset", width=100, height=5).pack()
+
+        self.settings_button = tk.Button(self.timing_canvas, text="Settings", width=100, height=5).pack()
+
+        def local_time_clock():
+            local_time = ctime()
+            digital_clock = local_time[11:19]
+            hours = int(local_time[11:13])
+
+            if hours > 12:
+                new_hours = hours - 12
+                digital_clock = digital_clock.replace(local_time[11:13], str(new_hours), 1)
+                digital_clock += " pm"
+
+            elif hours == 12:
+                digi_clock += " pm"
+
+            elif hours == 0:
+                new_hours = hours + 12
+                digital_clock = digital_clock.replace(local_time[11:13], str(new_hours), 1)
+                digital_clock += " am"
+
+
+            self.clock['text'] = digital_clock
+            self.main_window.after(1000, local_time_clock)
+
+        def exam_timer():
+            pass
+
+        local_time_clock()
+
+        # Run the main window
+        self.main_window.mainloop()
+
+# Run the program through the class 'ExamTimer'
+ExamTimer()
+
+
 
 
---
+++ test.py
@@ -0,0 +1,6 @@
+"""
+This python file contains the tests for the ExamTimer program
+These may or may not be implemented in the final version of the program
+This is just for testing purposes
+:)
+"""
