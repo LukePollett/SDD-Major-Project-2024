abf406a4b4e59936ed1bb936165a7d8cab63778f
Luke <lpollett@student.saintaug.nsw.edu.au>
2024-05-06 15:46:46 +1000

    From 30/12/23 - Dropdown menu on exam settings window including subjects and exam lengths(time) + Option for multiple exams -> want it to create new widgets/dropdowns accordingly

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 # Implemented Python Libraries:
 import tkinter as tk
+from tkinter import *
 import time
 from PIL import Image,ImageTk
 import math
 
 # Create the main class for the ExamTimer
-class ExamTimer():
+class ClockwiseScholar():
 
     # Initializing the main window
     def __init__(self):
@@ -19,7 +20,7 @@ class ExamTimer():
         self.main_window.state('zoomed')
         self.main_window.geometry("%dx%d" % (width, height))
 
-        self.main_window.title("ExamTimer")
+        self.main_window.title("Clockwise Scholar - Examination Management and Timing")
 
         # Create the canvas for the main window
         self.main_canvas = tk.Canvas(self.main_window, width=width, height=height, bg="white")
@@ -49,14 +50,229 @@ class ExamTimer():
             settings_window.wm_attributes('-topmost', 'True', )
             settings_window.overrideredirect(True)
 
-            settings_canvas = tk.Canvas(settings_window, width=700, height=700, bg="white")
+            settings_canvas = tk.Canvas(settings_window, width=700, height=700, bg="light grey")
             settings_canvas.pack()
 
-            l = tk.Label(settings_canvas, text="Edit Exam Details Here", font=('Helvetica Bold', 50), bg="white", fg="black")
-            l.place(relx=0.5, rely=0.5, anchor="center")
-
             b = tk.Button(settings_canvas, text="Done", font=('Helvetica Bold', 50), bg="white", fg="black", command=settings_window.destroy)
-            b.place(relx=0.5, rely=0.6, anchor="center")
+            b.place(relx=0.5, rely=0.9, anchor="center")
+
+            # Displaying the selected option as a label (temporary, will implement more widgets)
+            # Also check if the selected option is a valid subject
+                # (this excludes stuff like "<< Select an Option >>", "-- Mandatory Courses --", and "-- Elective Courses --")
+            def show():
+                if clicked.get() == "<< Select an Option >>": 
+                    pass
+                elif clicked.get() == "-- Mandatory Courses --":
+                    pass
+                elif clicked.get() == "-- Elective Courses --":
+                    pass
+                else:
+                    selected_subject.config(text = clicked.get()) 
+
+            # Subject Exam Selection for Stages 4 and 5 are kinda just there for placeholder purposes.
+            # This program is mainly for Stage 6, but I added the other stages just in case in the future,
+            # i want to have options to switch between year groups.
+
+            # Subject Exam Selection for Years 7 and 8
+            Stage_4_Subjects = [
+                "<< Select an Option >>",
+                "English",
+                "Geography",
+                "History",
+                "Chinese",
+                "Italian", 
+                "Japanese",
+                "Mathematics",
+                "Music",
+                "PDHPE",
+                "Religious Education",
+                "Science",
+                "Technology",
+                "Visual Arts"
+            ]
+
+            # Subject Exam Selection for Years 9 and 10
+            Stage_5_Subjects = [
+                "<< Select an Option >>",
+                "-- Mandatory Courses --",
+                "English",
+                "Geography",
+                "History",
+                "Mathematics",
+                "PDHPE",
+                "Religious Education",
+                "Science",
+                "-- Elective Courses --",
+                "Chinese",
+                "Chinese Native",
+                "Commerce",
+                "Design and Technology",
+                "Drama",
+                "History Elective",
+                "Industrial Technology - Engineering",
+                "Industrial Technology - Timber",
+                "Information and Software Technology",
+                "Chinese",
+                "Italian",
+                "Japanese",
+                "Music",
+                "Photographic and Digital Media",
+                "Physical Activity and Sports Studies",
+                "Visual Arts"
+            ]
+
+            # Subject Exam Selection for Years 11 and 12 (Preliminary and HSC)(State-Wide)
+            Stage_6_Subjects = [ 
+                "Aboriginal Studies", # 3hrs working, 5 mins reading
+                "Agriculture", # 3hrs working, 5 mins reading
+                "Ancient History", # 3hrs working, 5 mins reading
+                "Arabic Continuers", # 2hrs 50 mins working, 10 mins reading
+                "Arabic Extension", # 1hr 50 mins working, 10 mins reading
+                "Armenian Continuers", # 2hrs 30 mins working, 10 mins reading
+                "Automotive", # 2hrs working, 5 mins reading
+                "Biology", # 3hrs working, 5 mins reading
+                "Business Services", # 2hrs working, 5 mins reading
+                "Business Studies", # 3hrs working, 5 mins reading
+                "Chemistry", # 3hrs working, 5 mins reading
+                "Chinese Beginners", # 2hrs 30 mins working, 10 mins reading
+                "Chinese Continuers", # 2hrs 50 mins working, 10 mins reading
+                "Chinese Extension", # 1hr 50 mins working, 10 mins reading
+                "Chinese & Literature", # 2hrs 50 mins working, 10 mins reading
+                "Chinese in Context", # 2hrs 30 mins working, 10 mins reading
+                "Classical Greek Continuers", # 3hrs working, 5 mins reading
+                "Classical Greek Extension", # 1hr 50 mins working, 10 mins reading
+                "Classical Hebrew Continuers", # 3hrs working, 5 mins reading
+                "Classical Hebrew Extension", # 1hr 50 mins working, 10 mins reading
+                "Community & Family Studies", # 3hrs working, 5 mins reading
+                "Construction", # 2hrs working, 5 mins reading
+                "Croatian Continuers", # 2hrs 30 mins working, 10 mins reading
+                "Design & Technology", # 1hr 30 mins working, 5 mins reading
+                "Drama", # 1hr 30 mins working, 5 mins reading
+                "Earth & Environmental Science", # 3hrs working, 5 mins reading
+                "Economics", # 3hrs working, 5 mins reading
+                "Electrotechnology", # 2hrs working, 5 mins reading
+                "Engineering Studies", # 3hrs working, 5 mins reading
+                "English Advanced Paper 1", # 1hr 30 mins working, 10 mins reading
+                "English Advanced Paper 2", # 2hrs working, 5 mins reading
+                "English EAL/D Paper 1", # 1hr 30 mins working, 10 mins reading
+                "English EAL/D Paper 2", # 1 hr working, 5 mins reading
+                "English EAL/D Listening Paper", # 30 minutes working, including reading time
+                "English Extension 1", # 2hrs working, 10 mins reading
+                "English Standard Paper 1", # 1hr 30 mins working, 10 mins reading
+                "English Standard Paper 2", # 2hrs working, 5 mins reading
+                "English Studies", # 2hrs 30 mins working, 10 mins reading
+                "Entertainment Industry", # 2hrs working, 5 mins reading
+                "Filipino Continuers", # 2hrs 30 mins working, 10 mins reading
+                "Financial Services", # 2hrs working, 5 mins reading
+                "Food Technology", # 3hrs working, 5 mins reading
+                "French Beginners", # 2hrs 30 mins working, 10 mins reading
+                "French Continuers", # 2hrs 50 mins working, 10 mins reading
+                "French Extension", # 1hr 50 mins working, 10 mins reading
+                "Geography", # 3hrs working, 5 mins reading
+                "German Beginners", # 2hrs 30 mins working, 10 mins reading
+                "German Continuers", # 2hrs 50 mins working, 10 mins reading
+                "German Extension", # 1hr 50 mins working, 10 mins reading
+                "History Extension", # 2hrs working, 10 mins reading
+                "Hospitality - Food & Beverage", # 2hrs working, 5 mins reading
+                "Hospitality - Kitchen Operations & Cookery", # 2hrs working, 5 mins reading
+                "Human Services", # 2hrs working, 5 mins reading
+                "Indonesian Beginners", # 2hrs 30 mins working, 10 mins reading
+                "Indonesian Continuers", # 2hrs 50 mins working, 10 mins reading
+                "Indonesian Extension", # 1hr 50 mins working, 10 mins reading
+                "Indonesian & Literature", # 2hrs 50 mins working, 10 mins reading
+                "Industrial Technology - Automotive", # 1hr 30 mins working, 5 mins reading
+                "Industrial Technology - Electronics", # 1hr 30 mins working, 5 mins reading
+                "Industrial Technology - Graphics", # 1hr 30 mins working, 5 mins reading
+                "Industrial Technology - Metal & Engineering", # 1hr 30 mins working, 5 mins reading
+                "Industrial Technology - Multimedia", # 1hr 30 mins working, 5 mins reading
+                "Industrial Technology - Timber & Furniture", # 1hr 30 mins working, 5 mins reading
+                "Information Processes & Technology", # 3hrs working, 5 mins reading
+                "Information & Digital Technology", # 2hrs working, 5 mins reading
+                "Investigating Science", # 3hrs working, 5 mins reading
+                "Italian Beginners", # 2hrs 30 mins working, 10 mins reading
+                "Italian Continuers", # 2hrs 50 mins working, 10 mins reading
+                "Italian Extension", # 1hr 50 mins working, 10 mins reading
+                "Japanese Beginners", # 2hrs 30 mins working, 10 mins reading
+                "Japanese Continuers", # 2hrs 50 mins working, 10 mins reading
+                "Japanese Extension", # 1hr 50 mins working, 10 mins reading
+                "Japanese in Context", # 2hrs 30 mins working, 10 mins reading
+                "Korean Beginners", # 2hrs 30 mins working, 10 mins reading
+                "Korean Continuers", # 2hrs 50 mins working, 10 mins reading
+                "Korean & Literature", # 2hrs 50 mins working, 10 mins reading
+                "Korean in Context", # 2hrs 30 mins working, 10 mins reading
+                "Latin Continuers", # 3hrs working, 5 mins reading
+                "Latin Extension", # 2hrs working, 10 mins reading
+                "Legal Studies", # 3hrs working, 5 mins reading
+                "Mathematics Advanced", # 3hrs working, 10 mins reading
+                "Mathematics Extension 1", # 2hrs working, 10 mins reading
+                "Mathematics Extension 2", # 3hrs working, 10 mins reading
+                "Mathematics Standard 1", # 2hrs working, 10 mins reading
+                "Mathematics Standard 2", # 2hrs 30 mins working, 10 mins reading
+                "Modern Greek Beginners", # 2hrs 30 mins working, 10 mins reading
+                "Modern Greek Continuers", # 2hrs 50 mins working, 10 mins reading
+                "Modern Greek Extension", # 1hr 50 mins working, 10 mins reading
+                "Modern Hebrew Continuers", # 2hrs 50 mins working, 10 mins reading
+                "Modern History", # 3hrs working, 5 mins reading
+                "PDHPE", # 3hrs working, 5 mins reading
+                "Physics", # 3hrs working, 5 mins reading
+                "Primary Industries", # 2hrs working, 5 mins reading
+                "Retail Services", # 2hrs working, 5 mins reading
+                "Serbian Continuers", # 2hrs 30 mins working, 10 mins reading
+                "Society and Culture", # 2hrs working, 5 mins reading
+                "Software Design and Development", # 3hrs working, 5 mins reading
+                "Spanish Beginners", # 2hrs 30 mins working, 10 mins reading
+                "Spanish Continuers", # 2hrs 50 mins working, 10 mins reading
+                "Spanish Extension", # 1hr 50 mins working, 10 mins reading
+                "Studies of Religion 1", # 1hr 30 mins working, 5 mins reading
+                "Studies of Religion 2", # 3hrs working, 5 mins reading
+                "Swedish Continuers", # 2hrs 30 mins working, 10 mins reading
+                "Textiles and Design", # 1hr 30 mins working, 5 mins reading
+                "Tourism, Travel and Events", # 2hrs working, 5 mins reading
+                "Vietnamese Continuers", # 2hrs 50 mins working, 10 mins reading
+                "Visual Arts", # 1hr 30 mins working, 5 mins reading
+            ]
+
+            clicked = StringVar() 
+
+            clicked.set("<< Select an Option >>")
+
+            dropdown_width = len(max(Stage_6_Subjects, key=len))
+
+            def selected_num_of_exams():
+                class Examination():
+                    pass
+                
+            def num_of_exams():
+                if check_box_variable.get() == 1:
+                    one.place(relx=0.2, rely=0.15, anchor = W)
+                    two.place(relx=0.4, rely=0.15, anchor = W)
+                    three.place(relx=0.6, rely=0.15, anchor = W)
+                else:
+                    one.place_forget()
+                    two.place_forget()
+                    three.place_forget()
+
+            var = IntVar()
+            one = Radiobutton(settings_canvas, text="One Exam", font=("Helvetica Bold", 20), bg="light grey", fg="black", variable=var, value=1, command=selected_num_of_exams)
+            two = Radiobutton(settings_canvas, text="Two Exams", font=("Helvetica Bold", 20), bg="light grey", fg="black", variable=var, value=2, command=selected_num_of_exams)
+            three = Radiobutton(settings_canvas, text="Three Exams", font=("Helvetica Bold", 20), bg="light grey", fg="black", variable=var, value=3, command=selected_num_of_exams)
+
+            check_box_variable = IntVar()
+            multiple_exams_checkbox = tk.Checkbutton(settings_canvas, text='Multiple Exams', font=("Helvetica Bold", 20), bg="light grey", fg="black", variable=check_box_variable, onvalue=1, offvalue=0, command=num_of_exams)
+            multiple_exams_checkbox.place(relx=0.5, rely=0.1, anchor="center")
+
+            dropdown = OptionMenu(settings_canvas, clicked, *Stage_6_Subjects)
+            dropdown.config(width=dropdown_width, font=('Helvetica Bold', 20), bg="light grey", fg="black")
+            dropdown.place(relx=0.5, rely=0.2, anchor="center")
+            
+            # Create button, it will change label text 
+            button = Button(settings_canvas , text = "Confirm", font=("Helvetica Bold", 20), bg="light grey", border=0, command = show)
+            button.place(relx=0.5, rely=0.3, anchor="center")
+            
+            # Create Label 
+            selected_subject = Label(settings_canvas , text = " ",  font=("Helvetica Bold", 20), bg="light grey", fg="black") 
+            selected_subject.place(relx=0.5, rely=0.4, anchor="center")
+
 
         # The settings is where the exam supervisor can change the different exams taking place,
         # and the time for each exam. There will be no interactable widgets on the main display,
@@ -70,9 +286,6 @@ class ExamTimer():
 
         self.labelframe_text = tk.Label(self.labelframe, text="Subject/Exam Details Will Go Here", font=('Helvetica Bold', 20), fg="black", bg="white")
         self.labelframe_text.pack(padx=15, pady=15)
-
-        class Examination():
-            pass
 
         # Create analog clock face
         def Analog_Clock():      
@@ -180,4 +393,4 @@ class ExamTimer():
         self.main_window.mainloop()
 
 # Run the program through the class 'ExamTimer'
-ExamTimer()
+ClockwiseScholar()
