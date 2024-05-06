b1771a38104239d7311ad2843d0ba95d0d3d78ba
Luke <lpollett@student.saintaug.nsw.edu.au>
2024-05-06 12:55:06 +1000

    From 26/12/23 - substituting ctime() for time.localtime() + note-taking/minor testing.

--- test.py
+++ test.py
@@ -4,12 +4,17 @@ These may or may not be implemented in the final version of the program
 This is just for testing purposes
 :)
 """
-
+# So i essentially want to get the code for the analog clock and implement it into the main program
+# Preferrably into a canvas for organisation
 
 import tkinter as tk
 import time
 import math
 
+# USE FOR MAIN PROGRAM:
+print(time.localtime())
+# time.localtime() returns a struct_time object in the following form:
+# time.struct_time(tm_year=(year int), tm_mon=(month int), tm_mday=(day int), tm_hour=(hour int), tm_min=(min int), tm_sec=(sec int), tm_wday=(which day of the week is it (int form 1 - 7)), tm_yday=(day of the year int), tm_isdst=(is daylight savings time on? (int 0 or 1) maybe idk))
 
 def create_clock():
     root = tk.Tk()
@@ -28,9 +33,13 @@ def create_clock():
         second = current_time.tm_sec
 
         # Calculate angles for hour, minute, and second hands
-        hour_angle = math.radians((hour % 12) * 30 + minute / 2)
-        minute_angle = math.radians(minute * 6 + second / 10)
-        second_angle = math.radians(second * 6)
+        # This is the worst part.
+        # Ah so it calculates each hand's angle separately.
+        # So the hour hand is calculated by taking the hour and multiplying it by 30 (because there are 360 degrees in a circle and 12 (im guessing unique) hours in a day, so 360 / 12 = 30) and then adding the minute divided by 2 (because there are 60 minutes in an hour and 30 / 60 = 0.5) to account for the hour hand moving slightly as the minute hand moves.
+        
+        hour_angle = math.radians((hour % 12) * 30 + minute / 2) # the + minute / 2 makes the hour hand move slightly as the minute hand moves
+        minute_angle = math.radians(minute * 6 + second / 10) # the + second / 10 makes the minute hand move slightly as the second hand moves
+        second_angle = math.radians(second * 6) # the * 6 is because there are 360 degrees in a circle and 60 seconds in a minute, so 360 / 60 = 6, hence one second is 6 degrees
 
         # Draw clock face
         canvas.create_oval(50, 50, 350, 350)
